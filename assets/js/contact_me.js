3708f5c2ddf367b5fd58582b2a90f7acdca9897d
redbee <echernis@serv.externos.prismamp.com>
2025-12-16 19:35:36 -0300

    Publish site: update gh-pages from master

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -8,7 +8,7 @@ $(function() {
     submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
-	  var url = "https://formspree.io/" + "contacto@argitobras.com";
+	  var url = "https://formspree.io/" + "emmanuel.chernis@example.com";
       var name = $("input#name").val();
       var username = $("input#username").val();
       var email = $("input#email").val();
